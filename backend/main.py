8b0bdc8eb98c4268d4764a8af599dff05a9057eb
Abhishek Vishwakarma <aviswas7544007@gmail.com>
2025-11-17 08:46:55 +0530

    test

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+
+# Allow frontend on Render:
+origins = [
+    "http://localhost:3000",
+    "https://deployment1-frontend.onrender.com",
+    "*",  # remove * in production
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # Allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],         # GET, POST, PUT, DELETE, OPTIONS
+    allow_headers=["*"],         # Authorization, Content-Type...
+)
 app = FastAPI()
 
 @app.get("/ping")
